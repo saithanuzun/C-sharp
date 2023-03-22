e3f757f9ec5927ec4f556704535241316a03d6fc
sait han uzun <saithan.uzun@gmail.com>
2023-03-22 23:34:19 +0000

    Tuples

---
+++ Tuples/Program.cs
@@ -0,0 +1,45 @@
+﻿class Sample
+{
+    public Tuple<string, int> GetPersonDetails()
+    {
+        Tuple<string, int> Person = new Tuple<string, int>("sait", 22);
+
+        return Person;
+    }
+
+    public (int id, string name, string email) GetCustomerDetails()
+    {
+        return (1, "saithanuzun", "saithan.uzun@gmail.com");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tuple<string, int> Person = new Tuple<string, int>("sait", 22);
+
+        Sample sample = new();
+        Console.WriteLine(sample);
+        Console.WriteLine(sample.GetPersonDetails().Item1);
+
+        Console.WriteLine(sample.GetCustomerDetails());
+
+        // get details
+        (int id, string name, string email) cust = sample.GetCustomerDetails();
+        Console.WriteLine(cust.email);
+        Console.WriteLine(cust.id);
+        Console.WriteLine(cust.name);
+
+        // or 
+        (int id, string name, string email) = sample.GetCustomerDetails();
+        Console.WriteLine(email);
+        Console.WriteLine(id);
+        Console.WriteLine(name);
+
+        // discard the value by using underscore 
+        (int id2, string name2, _) = sample.GetCustomerDetails();
+        Console.WriteLine(id2);
+        Console.WriteLine(name2);
+    }
+}
