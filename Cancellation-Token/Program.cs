5900d64e881caa352ef85105be234defeba6298b
sait han uzun <saithan.uzun@gmail.com>
2023-04-16 23:14:50 +0100

    Cancellation-Token

---
+++ Cancellation-Token/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var source = new CancellationTokenSource();
+        var token = source.Token;
+        
+        var task = Task.Run(() => DoSomething(token), token);
+        
+        Thread.Sleep(500);
+        
+        Console.WriteLine("Main::Cancel");
+        source.Cancel();
+        
+        Thread.Sleep(500);
+    }
+    
+    static void DoSomething(CancellationToken token)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Thread.Sleep(100);
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Method1 canceled");
+                return;
+            }
+            Console.WriteLine($"Method1 running... {i}");
+        }
+    }
+}
