cfd3972914e5d3249fdde5556b7f0d92a80760b9
sait han uzun <saithan.uzun@gmail.com>
2023-04-16 23:09:01 +0100

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    private static async Task Main()
+    {
+        Console.WriteLine("step 1");
+        var myTask = GetContent();
+        Console.WriteLine("step 2");
+
+        var content = await myTask;
+        Console.WriteLine("step 3 = " + content.Length);
+        
+        
+    }
+
+    private static async Task<string> GetContent()
+    {
+        var content = await  new HttpClient().GetStringAsync("https://www.google.com");
+        Console.WriteLine("get content");
+        return   content;
+    }
+}
