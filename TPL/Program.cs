2e8d336088e72964c6c0a1038271a563da1110cd
sait han uzun <saithan.uzun@gmail.com>
2023-04-17 21:37:38 +0100

    TPL

---
+++ TPL/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program 
+{
+    static void Main() 
+    {
+        // Multithreading
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Parallel.ForEach(numbers, (number) => {
+            
+            // Process each number in parallel
+            
+            Console.WriteLine("Processing number {0} on thread {1}", number, Task.CurrentId);
+            Thread.Sleep(new Random().Next(100, 1000));
+        });
+
+        Console.WriteLine("All numbers processed.");
+
+    }
+}
