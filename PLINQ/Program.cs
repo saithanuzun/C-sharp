16cb4f6b3d6b6f728003ad3a275617c0a9589aff
sait han uzun <saithan.uzun@gmail.com>
2023-04-18 23:51:33 +0100

    PLINQ

---
+++ PLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PLINQ;
+
+public class Program
+{
+    private static void Main(String[] args)
+    {
+        List<int> numbers = Enumerable.Range(1, 100).ToList();
+
+        var evenNumbers = numbers.AsParallel()
+            .Where(n => n % 2 == 0)
+            .ToList();
+
+        Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+        
+        // or 
+
+        var array = Enumerable.Range(1, 100).ToList();
+
+        var newArray = array.AsParallel().Where(x => x % 2 == 0);
+        
+        newArray.ToList().ForEach(WriteToConsole);
+    }
+
+    private static void WriteToConsole(int x)
+    {
+        Console.WriteLine(x);
+    }
+}
