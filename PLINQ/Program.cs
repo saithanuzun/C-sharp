66a8640015fe4260d05724978f3cf8c9ca3abf1a
sait han uzun <saithan.uzun@gmail.com>
2023-04-19 22:10:26 +0100

    PLINQ

--- PLINQ/Program.cs
+++ PLINQ/Program.cs
@@ -11,14 +11,34 @@ public class Program
             .ToList();
 
         Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
-        
+
         // or 
 
         var array = Enumerable.Range(1, 100).ToList();
 
         var newArray = array.AsParallel().Where(x => x % 2 == 0);
-        
+
         newArray.ToList().ForEach(WriteToConsole);
+
+        //example 2
+
+        var tasks = Enumerable.Range(0, 10).Select(id => Task.Run(async () => await HeavyTask(id)));
+
+        var results = tasks
+            .AsParallel()
+            .WithDegreeOfParallelism(3)
+            .Select(t => t.Result)
+            .ToList();
+
+        results.ForEach(Console.WriteLine);
+    }
+
+    static async Task<int> HeavyTask(int id)
+    {
+        Console.WriteLine("Starting Task #" + id);
+        await Task.Delay(new Random().Next(3000, 10000));
+        Console.WriteLine("Finished Task #" + id);
+        return id;
     }
 
     private static void WriteToConsole(int x)
