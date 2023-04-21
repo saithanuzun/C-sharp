92eff3771098da1112c22ef7664faf774040e9b8
sait han uzun <saithan.uzun@gmail.com>
2023-04-21 18:17:03 +0100

    Regular-Expressions

---
+++ Regular-Expressions/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * 
+ */
+using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+        string? value = Console.ReadLine();
+        bool match = regex.IsMatch(value);
+        Console.WriteLine(match);
+    }
+}
